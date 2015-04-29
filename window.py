4b2781ea7aed1580a7bcdc4dc49b6c567de507f7
dan <danbickford007@yahoo.com>
2015-04-28 22:32:13 -0400

    starting menu

--- window.py
+++ window.py
@@ -9,12 +9,36 @@ class Window:
     top = Tkinter.Tk()
     self.set_top(top)
     self.set_text()
+    self.menu()
     top.geometry('{}x{}'.format(600, 600))
     B = Tkinter.Button(top, text ="Select a directory to inspect:", command = self.start)
     self.var.set('Select a directory to inspect...')
 
     B.pack()
     top.mainloop()
+
+  def menu(self):
+    menubar = Menu(self.top)
+
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label="todo", command="")
+    filemenu.add_command(label="exit", command=self.top.quit)
+    menubar.add_cascade(label="Filer", menu=filemenu)
+    self.top.config(menu=menubar)
+#     mb=  Menubutton ( self.top, text="menu", relief=RAISED )
+#     mb.grid()
+#     mb.menu  =  Menu ( mb, tearoff = 0 )
+#     mb["menu"]  =  mb.menu
+#     mb.pack()
+#
+#     mb.add_command(label="Quit", command="sys.exit(0)")   
+
+#  
+#     mb.menu.add_checkbutton ( label="look for offenders",
+#                           variable=1 )
+#     mb.menu.add_checkbutton ( label="exit",
+#                               variable="exit()" )
+
 
   def set_top(self, top):
     self.top = top
